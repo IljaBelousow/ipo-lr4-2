fe14114ab0491c30081b9efc174dc6e21bcbfadf
ПИКМИ МЕН <103372208+IljaBelousow@users.noreply.github.com>
2024-11-22 15:24:14 +0300

    Add files via upload

---
+++ ipo-lr4-2.py
@@ -0,0 +1,8 @@
+spisok = []
+num = int(input("vvedite kolichestvo chisel "))
+for i in range(num): #запрашивает числа num раз и добавляет их в список
+    a = int(input("chislo "))
+    spisok.append(a)
+print(spisok)
+spisok2 = [x ** 2 for x in spisok] #каждое число в списке возводит в квадрат
+print(spisok2)
