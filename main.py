0201464cbddee203551acdfecbc1c52d1acbf78c
ПИКМИ МЕН <103372208+IljaBelousow@users.noreply.github.com>
2024-12-21 11:47:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 spisok = []
-num = int(input("vvedite kolichestvo chisel "))
+num = int(input("ведите колво числе "))
 for i in range(num): #запрашивает числа num раз и добавляет их в список
-    a = int(input("chislo "))
+    a = int(input("число "))
     spisok.append(a)
 spisok2 = [x ** 2 for x in spisok] #каждое число в списке возводит в квадрат
 print(spisok2)
