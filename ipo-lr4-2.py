495101ec6b91446c6d221d3fc1ed590bf58abefb
ПИКМИ МЕН <103372208+IljaBelousow@users.noreply.github.com>
2024-12-21 11:47:07 +0300

    Delete ipo-lr4-2.py

--- ipo-lr4-2.py
+++
@@ -1,8 +0,0 @@
-spisok = []
-num = int(input("введите количество чисел"))
-for i in range(num): #запрашивает числа num раз и добавляет их в список
-    a = int(input("chislo "))
-    spisok.append(a)
-print(spisok)
-spisok2 = [x ** 2 for x in spisok] #каждое число в списке возводит в квадрат
-print(spisok2)
